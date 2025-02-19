565264a8cc520702c9be08dcf5c4987046455db7
szymo <szymon.basiul@wp.pl>
2025-02-19 19:39:59 +0100

    small fix

--- src/app/pages/Contact.tsx
+++ src/app/pages/Contact.tsx
@@ -1,20 +1,21 @@
 export default function Contact() {
-	return (
-		<section id="contact" className="contact">
-			<footer className=" bg-beigeLight text-dark text-center p-4">
+  return (
+    <section id="contact" className="contact">
+      <footer className=" bg-beigeLight text-dark text-center p-4">
         <h2 className="section-title container pt-[2em] pb-[1em] text-dark text-4xl md:text-3xl sm:text-2xl text-center font-bold uppercase">Kontakt</h2>
         <div className="container p-4 mt-[2em] text-base text-justify md:text-lg sm:text-md leading-relaxed">
-        <form action="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae sunt nulla assumenda expedita doloremque quas quos quaerat voluptatibus, delectus ut distinctio perferendis placeat eum quo nostrum ex cumque itaque dolor!
-        Error corporis minus alias sed excepturi cum molestiae? Consequuntur eaque pariatur placeat quae exercitationem soluta. Laudantium necessitatibus, voluptatem iure officia, nulla, pariatur earum sit facere ad facilis architecto atque recusandae!
-        Dicta obcaecati perferendis eveniet deserunt voluptate a dolor, similique quisquam tenetur corrupti placeat quod expedita necessitatibus facilis ad ipsa repellendus nostrum eum cupiditate nihil! Nobis natus maiores in nam fugiat.
-        Itaque, explicabo cupiditate voluptate repudiandae sit quis inventore ea molestiae consequuntur asperiores laboriosam illum eos corporis iste, eius labore non in exercitationem eveniet illo odit incidunt! Porro culpa doloremque voluptate.
-        Perspiciatis quaerat et explicabo accusantium a facilis sed eaque tempore nostrum? Corrupti adipisci qui eos illo? Distinctio tempora, saepe fuga quia ad maiores. Modi voluptate quod, ducimus ipsa omnis ex!
-        Molestias vero cupiditate porro rerum eum facere fugiat. Cupiditate perspiciatis placeat facilis autem qui possimus repudiandae voluptatibus, debitis ut tempore provident quam dolore ipsum, totam ipsam impedit saepe distinctio incidunt.
-        Reiciendis ullam sapiente minima dignissimos? Nihil culpa eligendi vel, asperiores ducimus quam totam neque natus eius consequuntur minus quas aut debitis quis? Quasi doloremque neque odio voluptatibus veniam animi ipsum!
-        Excepturi architecto dolorem ipsa quis veritatis laborum possimus pariatur itaque ab ratione voluptatum error, perferendis maiores ducimus reprehenderit esse ut temporibus tempora ipsum, non sint saepe quasi nulla. Laudantium, molestias!
-        Voluptatem eum facere saepe quisquam, officiis earum illo repellendus porro soluta, veniam perferendis eaque. Laudantium doloremque, odit nisi, et rerum dicta ullam hic minus a similique laborum blanditiis, libero voluptatibus!
-        Inventore molestias labore animi possimus! Unde quasi voluptas totam sit ducimus quas architecto culpa? Modi ipsam quibusdam eaque asperiores reprehenderit ullam, optio laboriosam hic quisquam repellat tempora, deserunt odio? Nisi.</form>
+          <form action="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae sunt nulla assumenda expedita doloremque quas quos quaerat voluptatibus, delectus ut distinctio perferendis placeat eum quo nostrum ex cumque itaque dolor!
+            Error corporis minus alias sed excepturi cum molestiae? Consequuntur eaque pariatur placeat quae exercitationem soluta. Laudantium necessitatibus, voluptatem iure officia, nulla, pariatur earum sit facere ad facilis architecto atque recusandae!
+            Dicta obcaecati perferendis eveniet deserunt voluptate a dolor, similique quisquam tenetur corrupti placeat quod expedita necessitatibus facilis ad ipsa repellendus nostrum eum cupiditate nihil! Nobis natus maiores in nam fugiat.
+            Itaque, explicabo cupiditate voluptate repudiandae sit quis inventore ea molestiae consequuntur asperiores laboriosam illum eos corporis iste, eius labore non in exercitationem eveniet illo odit incidunt! Porro culpa doloremque voluptate.
+            Perspiciatis quaerat et explicabo accusantium a facilis sed eaque tempore nostrum? Corrupti adipisci qui eos illo? Distinctio tempora, saepe fuga quia ad maiores. Modi voluptate quod, ducimus ipsa omnis ex!
+            Molestias vero cupiditate porro rerum eum facere fugiat. Cupiditate perspiciatis placeat facilis autem qui possimus repudiandae voluptatibus, debitis ut tempore provident quam dolore ipsum, totam ipsam impedit saepe distinctio incidunt.
+            Reiciendis ullam sapiente minima dignissimos? Nihil culpa eligendi vel, asperiores ducimus quam totam neque natus eius consequuntur minus quas aut debitis quis? Quasi doloremque neque odio voluptatibus veniam animi ipsum!
+            Excepturi architecto dolorem ipsa quis veritatis laborum possimus pariatur itaque ab ratione voluptatum error, perferendis maiores ducimus reprehenderit esse ut temporibus tempora ipsum, non sint saepe quasi nulla. Laudantium, molestias!
+            Voluptatem eum facere saepe quisquam, officiis earum illo repellendus porro soluta, veniam perferendis eaque. Laudantium doloremque, odit nisi, et rerum dicta ullam hic minus a similique laborum blanditiis, libero voluptatibus!
+            Inventore molestias labore animi possimus! Unde quasi voluptas totam sit ducimus quas architecto culpa? Modi ipsam quibusdam eaque asperiores reprehenderit ullam, optio laboriosam hic quisquam repellat tempora, deserunt odio? Nisi.</form>
         </div>
-			</footer>
-		</section>
-	);
+      </footer>
+    </section>
+  );
+}
